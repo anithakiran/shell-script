96b202072b7264dda284f62a8ee544cbb99943f5
anithakiran <anitha1995d@gmail.com>
2024-05-05 14:46:10 +0530

    first commit

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter user name :: "
+
+read USERNAME 
+
+echo "please enter password :: "
+
+read PASSWORD
+
+echo "USERNAME is : $USERNAME"
+echo "PASSWORD is : $PASSWORD"
+
