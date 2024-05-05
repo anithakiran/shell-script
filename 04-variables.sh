15da73acf9b6e3958bf915c12c04680c48702540
anithakiran <anitha1995d@gmail.com>
2024-05-05 14:41:37 +0530

    first commit

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hello $PERSON2, How are you?"
+echo "$PERSON2 :: Hi $PERSON1, Iam fine, How are you ?"
+echo "$PERSON1 :: Iam fine too... How is your work?"
+echo "$PERSON2 :: Not bad. Iam thinking to upgrade to Devops"
