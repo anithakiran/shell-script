36315e70bf5b5ebe0d16c22e6762561080d8454a
anithakiran <anitha1995d@gmail.com>
2024-05-05 13:53:22 +0530

    first commit

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+UserID=$(id -u)
+
+if [$UserID -ne 0]
+then 
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "your are super user"
+fi
+
 dnf install mysql -y
+echo "is script processing?"
+
+if [$? -ne 0]
+then    
+    echo "installation is failure"
+else
+    echo "installation is successfull"
+fi
