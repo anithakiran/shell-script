e9d1a40a4a541cf780e4dcbe9e79543fe2ab3984
anithakiran <anitha1995d@gmail.com>
2024-05-05 15:21:57 +0530

    first commit

---
+++ 12-fun-logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+LOGFILE = /tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+USERID=$(id -u)
+
+VALIDATE() {
+if [ $? -ne 0 ]
+then    
+    echo "installation is failure"
+else
+    echo "installation is successfull"
+fi
+}
+
+if [ $UserID -ne 0 ]
+then 
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "your are super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+
+
