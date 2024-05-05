728ef79fad124a09a6117104b764756dd71c8397
anithakiran <anitha1995d@gmail.com>
2024-05-05 14:53:11 +0530

    first commit

---
+++ 06-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WEEKS=("SUNDAY" "MONDAY" "TUESDAY" "WEDNESDAY" "THURSDAY" "FRIDAY" "SATURDAY")
+
+echo "First day of the week is : ${WEEKS[0]}"
+echo "Third day of the week is : ${WEEKS[3]}" 
+echo "Days of the week :${WEEKS[@]}"
+echo "last day of the week is : ${WEEKS[6]}
