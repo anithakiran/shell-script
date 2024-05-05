f55894f6a0d2f5b8469d6de0b541cc87e9c74787
anithakiran <anitha1995d@gmail.com>
2024-05-05 16:26:23 +0530

    first commit

--- 13-loop.sh
+++ 13-loop.sh
@@ -7,7 +7,7 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
-Y="e[33m"
+Y="\e[33m"
 N="\e[0m"
 
 echo "script started executing at : $TIMESTAMP"
