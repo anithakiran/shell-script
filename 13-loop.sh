d7104bdb977813100d44928bd21bd24be0b025b2
anithakiran <anitha1995d@gmail.com>
2024-05-05 15:53:45 +0530

    first commit

---
+++ 13-loop.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+LOGFILE = /tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="e[33m"
+N="\e[0m"
+
+echo "script started executing at : $TIMESTAMP"
+
+VALIDATE() {
+if [ $? -ne 0 ]
+then    
+    echo -e "installation is $R failure $N "
+else
+    echo -e "installation is $G successfull $N"
+fi
+}
+
+if [ $UserID -ne 0 ]
+then 
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "your are super user"
+fi
+
+for i in $@
+do 
+    echo "package o install : $i"
+    dnf list installed $i &>> $LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed... $Y SKIPPING $N"
+    else
+        dnf  install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done
---
+++ 14-colors.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
+LOGFILE = /tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "script started executing at : $TIMESTAMP"
+
+VALIDATE() {
+if [ $? -ne 0 ]
+then    
+    echo -e "installation is $R failure $N "
+else
+    echo -e "installation is $G successfull $N"
+fi
+}
+
+if [ $UserID -ne 0 ]
+then 
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "your are super user"
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing git"
