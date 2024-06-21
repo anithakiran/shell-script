ee0dee01666e2ef70ca6e589d0888bb74f7e9232
anithakiran <anitha1995d@gmail.com>
2024-06-21 19:39:17 +0530

    practice

---
+++ add-two-numbers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if[$# -ne 2]
+then
+    echo "please only two numbers only"
+    echo "please enter values has shown ---> script_name.sh  x y"
+    echo "so x and y values will be added and sum values be given"
+fi
+
+echo "sum of two numbers is 'expr $1 + $2'"
