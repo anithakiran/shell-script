0ff2ba26a3d6fa1ce5e528912047009b22f32acb
anithakiran <anitha1995d@gmail.com>
2024-06-22 13:53:13 +0530

    practice

--- print-number-reverse.sh
+++ print-number-reverse.sh
@@ -4,17 +4,14 @@ echo enter n
 
 read n
 
-num=0
+sd=0
+rev=0
 while [ $n -gt 0 ]
 do 
-#num=$(expr $num \* 10)
-k=$(expr $n % 10)
-num=$(expr $num + $k)
-n=$(expr $n / 10)
+sd=`expr $n %10`
+rev=`expr $rev \* 10 + $sd`
+n=`expr $n /10`
 done
 echo number is $num
 
 
-#10572
-#10572 % 2 = 2
-#
