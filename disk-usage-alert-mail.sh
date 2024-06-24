2d7f56711b934d60ad07e6802edf8bccfc33bd7e
anithakiran <anitha1995d@gmail.com>
2024-06-24 09:25:17 +0530

    practice

--- disk-usage-alert-mail.sh
+++ disk-usage-alert-mail.sh
@@ -8,7 +8,7 @@ while IFS= read -r line
 do
     
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    FOLDER=$(echo $line |awk -F " " '{print $NF}')
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOULD]
     then
          MESSAGE+= "$FOLDER is more than  $DISK_THRESHOULD ,current usage: $USAGE \n"
