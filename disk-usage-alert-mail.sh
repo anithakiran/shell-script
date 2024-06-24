3ee638722b006ddd4bae9ac7912f82836894fe13
anithakiran <anitha1995d@gmail.com>
2024-06-24 09:35:57 +0530

    practice

--- disk-usage-alert-mail.sh
+++ disk-usage-alert-mail.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=6
-MESSAGE=""
+DISK_THRESHOULD=5
+MESSAGE=" "
 
 while IFS= read -r line
 do
+    
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
-    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
+    FOLDER=$(echo $line | awk -F " " '{print $NF}' )
+    if [ $USAGE -ge $DISK_THRESHOULD ]
     then
-        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+         MESSAGE+="$FOLDER is more than  $DISK_THRESHOULD current usage: $USAGE \n"
     fi
 done <<< $DISK_USAGE
 
-echo -e "Message: $MESSAGE"
+echo -e "Message : $MESSAGE"
