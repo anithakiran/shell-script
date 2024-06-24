36a757391a24bc3b965c974a98083880644e401f
anithakiran <anitha1995d@gmail.com>
2024-06-24 06:07:15 +0530

    practice

---
+++ set-cmd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run the script using root access........"
+    exit 1
+else 
+    echo "your super user....."
+fi
+
+dnf install mysql -y
+dnf install gitt -y
+dnf install dockker -y
+
+echo "is still script working ?..........."
