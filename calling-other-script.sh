e83399bf94c925490b8e621d27b33f186739f0d5
anithakiran <anitha1995d@gmail.com>
2024-06-24 05:58:45 +0530

    practice

--- calling-other-script.sh
+++ calling-other-script.sh
@@ -5,6 +5,7 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./other-script.sh
+#./other-script.sh
 
+source ./other-script.sh
 echo "After calling other script, course: $COURSE"
