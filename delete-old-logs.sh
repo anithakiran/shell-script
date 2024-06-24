ee95f1d1bc545b7e88c623f3cf8af61c5aa62153
anithakiran <anitha1995d@gmail.com>
2024-06-24 06:43:32 +0530

    practice

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-source_directory=/tmp/app-logs
+SOURCE_DIRECTORY=/tmp/app-logs
+
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
-if [ -d $source_directory ]
-then 
-    echo -e "$G soruce_directory exist $N"
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G Source directory exists $N"
 else
-    echo -e "$R pleaase make sure $source_directory exist $N"
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
     exit 1
 fi
 
-
-FILES=$(find $soruce_directory -name "*.log" -mtime +14)
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
 while IFS= read -r line
 do
-    echo "deleting file: $line"
+    echo "Deleting file: $line"
     rm -rf $line
 done <<< $FILES
-
