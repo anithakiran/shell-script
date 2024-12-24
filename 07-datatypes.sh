e344c3677a562669dfe54aad91197d88cefba0e7
anithakiran <anitha1995d@gmail.com>
2024-12-24 14:45:25 +0530

    updated code

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -4,7 +4,7 @@ No1=$1
 No2=$2
 
 SUM=$(($No1+$No2))
-AVG=$(($No1/$No2))
+SUB=$(($No1-$No2))
 
 echo "Total of $No1 and $No2 is : $SUM"
-echo "Average of $No1 and $No2 is : $AVG"
+echo "Average of $No1 and $No2 is : $SUB"
